5ffd63f4092c975564945172316e3eaa6a9b899e
ram global <ramchary11@gmail.com>
2026-05-13 16:49:07 +0530

    chore: docker environment setup for PySpark + MLflow

---
+++ spark_test.py
@@ -0,0 +1,13 @@
+﻿from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master('local') \
+    .appName('test') \
+    .config('spark.jars.packages', 'io.delta:delta-core_2.12:2.4.0') \
+    .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension') \
+    .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog') \
+    .getOrCreate()
+
+spark.createDataFrame([('ok',)], ['status']).show()
+spark.stop()
+print('Spark OK')
